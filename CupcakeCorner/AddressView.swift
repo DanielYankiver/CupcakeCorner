1c09917a1f4251b982da93a326d733ee40d46401
Daniel Yankiver <dyankiver@gmail.com>
2023-12-28 15:59:55 -0500

    setting up order details and address view

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Daniel Yankiver on 12/28/23.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+  var order: Order
+
+  var body: some View {
+    Text("Hello")
+  }
+}
+
+#Preview {
+  AddressView(order: Order())
+}
